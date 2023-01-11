8ea7bd51e455037494c6cd78f3b9bf0e36e1a2ce
mostofa kamal <mostofakamal1990@gmail.com>
2023-01-11 23:55:37 +0600

    Event handler

--- AdvanceCshapr/Classes/Foo.cs
+++ AdvanceCshapr/Classes/Foo.cs
@@ -9,6 +9,10 @@ namespace AdvanceCshapr.Classes
 {
     public class Foo : IFoo
     {
+        public static event EventHandler<EventArgs> StaticEvent;
+
+        public virtual event EventHandler<EventArgs> VirtualEvent;
+
         public event EventHandler ev;
 
         event EventHandler IFoo.Ev
@@ -17,6 +21,61 @@ namespace AdvanceCshapr.Classes
             remove { ev -= value; }
         }
 
-       
+        Func<int, int> sqr = x => x * x;
+
+        Func<string, string, int> totalLength = (s1, s2) => s1.Length + s2.Length;
+        //  int total = totalLength("hello", "world"); // total is 10;
+
+        Func<string> greetor = () => "Hello, world";
+        Func<string> Greeter = () => "Hello, world";
+
+        void Foos<T>(T x) { }
+        void Bar<T>(Action<T> a)
+        {
+
+        }
+        //void  Barr<T>(x => Foo(x)); // Specify type parameter for Bar
+        //void   Barrr<T>(Foo); // As above, but with method group
+
+        public int Sqr(int x) => x * x;
+        static int factor = 2;
+        Func<int, int> multiplier = n => n * factor;
+
+        static int seed = 0;
+        Func<int> natural = () => seed++;
+
+        static Func<int> Natural()
+        {
+            int seed = 0;
+            return () => seed++; // Returns a closure
+        }
+
+        public event EventHandler Clicked = delegate { };
+
+        void ReadFile()
+        {
+            StreamReader reader = null; // In System.IO namespace
+            try
+            {
+                reader = File.OpenText("file.txt");
+                if (reader.EndOfStream) return;
+                Console.WriteLine(reader.ReadToEnd());
+            }
+            finally
+            {
+                if (reader != null) reader.Dispose();
+            }
+        }
+
+
+        void Display(string name)
+        {
+            if (name == null)
+                throw new ArgumentNullException(nameof(name));
+            Console.WriteLine(name);
+        }
+
+
+
     }
 }
--- AdvanceCshapr/Program.cs
+++ AdvanceCshapr/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 
+using AdvanceCshapr.Classes;
 using AdvanceCshapr.Delegates;
 
 CalculationDelegateClass delC = new CalculationDelegateClass();
@@ -11,8 +12,8 @@ Transformer tran = delC.Multiply;
 //Console.WriteLine(tran.Invoke(5));
 var values = new int[] { 3, 4, 5, 6, 7, 8 };
 
- //delC.Transform(values, trans);
- //delC.Transform(values, tran);
+//delC.Transform(values, trans);
+//delC.Transform(values, tran);
 
 
 //foreach (var v in values) Console.WriteLine(v);
@@ -31,4 +32,15 @@ p += util.WriteProgressToFile;
 //foreach(var v in values) Console.Write(v + " , ");
 
 Util.TransformAll(values, new Squarer());
-foreach (var v in values) Console.Write(v + " , ");
+//foreach (var v in values) Console.Write(v + " , ");
+
+
+Action[] actions = new Action[3];
+
+//for (int i = 0; i < 3; i++)
+//    actions[i] = () => Console.Write(i);
+//foreach (Action a in actions) a(); // 333
+
+
+// Notice that we omit the parameters:
+new Foo().Clicked += delegate { Console.WriteLine("clicked"); };
