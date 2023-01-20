22a7f16add7cefe9ed07aa8e2179b16f7c772479
mostofa kamal <mostofakamal1990@gmail.com>
2023-01-20 22:24:19 +0600

    Collections of interfaces

---
+++ CSharpCollection/IEnumerators.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCollection
+{
+    public interface IEnumerators
+    {
+        bool MoveNext();
+        object Current { get; }
+        void Reset();
+    }
+
+    public interface IEnumerable
+    {
+        IEnumerator GetEnumerator();
+    }
+}
---
+++ CSharpCollection/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CSharpCollection;
+using System.Collections;
+
+string s = "Hello";
+// Because string implements IEnumerable, we can call GetEnumerator():
+IEnumerator rator = s.GetEnumerator();
+while (rator.MoveNext())
+{
+    char c = (char)rator.Current;
+    Console.Write(c + ".");
+}
+
+foreach (char c in s)
+    Console.Write(c + ".");
